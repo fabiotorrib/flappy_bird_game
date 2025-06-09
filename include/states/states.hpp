7f40834b85f6372b2eb065bd9121c6ccd4880d1f
fabiotorrib <fabioluis2345@gmail.com>
2025-06-09 18:22:33 -0300

    corrigindo game object

--- include/states/states.hpp
+++ include/states/states.hpp
@@ -2,7 +2,7 @@
 #include "main_menu.hpp"
 #include "play.hpp"
 #include "load_game.hpp"
-#include "motion.hpp"
+#include "../motion.hpp"
 
 //criando e distribuindo os objetos globais de cada estado
 inline MainMenu mainMenu;
