71ace41e5acb4afed509e5c4a6c149ee3f15ef72
fabiotorrib <fabioluis2345@gmail.com>
2025-06-09 18:15:47 -0300

    correcao do game object

--- obj/init.d
+++
@@ -1,5 +0,0 @@
-obj/init.o: src/init.cpp src/../include/init.hpp \
- src/../include/defines.hpp src/../include/state.hpp
-src/../include/init.hpp:
-src/../include/defines.hpp:
-src/../include/state.hpp:
--- obj/state.d
+++
@@ -1,4 +0,0 @@
-obj/state.o: src/state.cpp src/../include/state.hpp \
- src/../include/defines.hpp
-src/../include/state.hpp:
-src/../include/defines.hpp:
