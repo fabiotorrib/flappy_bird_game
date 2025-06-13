1cac6af4d05740260ccd3a7334b6099099cf4874
goatt-t <goat0705@proton.me>
2025-06-13 20:35:52 -0300

    modified .gitignore

--- obj/states/play.d
+++
@@ -1,7 +0,0 @@
-obj/states/play.o: src/states/play.cpp \
- src/states/../../include/states/play.hpp \
- src/states/../../include/states/../state.hpp \
- src/states/../../include/states/../defines.hpp
-src/states/../../include/states/play.hpp:
-src/states/../../include/states/../state.hpp:
-src/states/../../include/states/../defines.hpp:
