3a85c8587edd998beb834e9dbdb25727eb046a02
Matheus Gregorio <n0scroll776@gmail.com>
2025-06-19 20:45:55 -0300

    Implementando animação de chuva e organizando  a classe motion com smart pointer

--- include/motion.hpp
+++ include/motion.hpp
@@ -1,11 +1,22 @@
 #pragma once
 #include <allegro5/allegro_primitives.h>
 #include "defines.hpp"
+#include "assets.hpp"
 
 class Motion {
+
   // Posição e velocidade de cada imagem
  private:
-  
+  int controller = 3;
+
+  float controll = 0;
+  float contF = 0;
+  float contB = 400;
+
+  float speedRain = 6;
+  float positionR1 = 400;
+  float positionR2 = -320;
+
   float speedFlakes = 1.0;
   float positionF = 500;
   float positionF2 = 550;
@@ -28,10 +39,33 @@ class Motion {
   float positionF2_x = 0;
   float speed_floor = 2.5;
 
- public:
-  void draw();
-  void update();
+  std::unique_ptr<Image> background_rain;
+  std::unique_ptr<Image> drips1;
+  std::unique_ptr<Image> drips2;
+  std::unique_ptr<Image> thunder;
+  std::unique_ptr<Image> thunder1;
+  std::unique_ptr<Image> background_snow;
+  std::unique_ptr<Image> flakesLittle;
+  std::unique_ptr<Image> flakesLittle2;
+  std::unique_ptr<Image> flakesBig;
+  std::unique_ptr<Image> background;
+  std::unique_ptr<Image> lights;
+  std::unique_ptr<Image> clouds;
+  std::unique_ptr<Image> clouds2;
+  std::unique_ptr<Image> little;
+  std::unique_ptr<Image> little2;
+  std::unique_ptr<Image> little3;
+  std::unique_ptr<Image> bird1;
+  std::unique_ptr<Image> bird2;
+  std::unique_ptr<Image> bird3;
+  std::unique_ptr<Image> ground;
+  std::unique_ptr<Image> ground2;
 
-  void snow_draw();
-  void snow_update();
+  public:
+    void draw();
+    void update();
+
+    Motion();
+
+    void setterControll(int);
 };
--- src/motion.cpp
+++ src/motion.cpp
@@ -2,90 +2,91 @@
 #include <allegro5/allegro_primitives.h>
 #include "../include/defines.hpp"
 #include "../include/init.hpp"
+#include "assets.hpp"
+#include <memory>
+
+//Inicializa as imagens do motion
+Motion::Motion(){
+
+  background_rain = std::make_unique<Image>("assets/background_rain.png");
+  drips1 = std::make_unique<Image>("assets/Drips.png");
+  drips2 = std::make_unique<Image>("assets/Drips.png");
+  thunder = std::make_unique<Image>("assets/Thunder.png");
+  thunder1 = std::make_unique<Image>("assets/Thunder1.png");
+
+  background_snow = std::make_unique<Image>("assets/background_snow.png");
+  flakesLittle = std::make_unique<Image>("assets/flakesLittle.png");
+  flakesLittle2 = std::make_unique<Image>("assets/flakesLittle.png");
+  flakesBig = std::make_unique<Image>("assets/flakesBig.png");
+
+  background = std::make_unique<Image>("assets/background.png");
+  lights = std::make_unique<Image>("assets/LightStrip.png");
+  clouds = std::make_unique<Image>("assets/Clouds.png");
+  clouds2 = std::make_unique<Image>("assets/Clouds.png");
+  little = std::make_unique<Image>("assets/Frame1.png");
+  little2 = std::make_unique<Image>("assets/Frame2.png");
+  little3 = std::make_unique<Image>("assets/Frame3.png");
+  bird1 = std::make_unique<Image>("assets/B1.png");
+  bird2 = std::make_unique<Image>("assets/B2.png");
+  bird3 = std::make_unique<Image>("assets/B3.png");
+  ground = std::make_unique<Image>("assets/Ground.png");
+  ground2 = std::make_unique<Image>("assets/Ground.png");
+
+}
+
+void Motion::setterControll(int x){
+  controller = x;
+}
 
 // Animação de todos sprites da tela de fundo
 void Motion::update() {
 
-  // Altera a posição do feixe de luz
-  positionL_x = positionL_x - speed_light;
+  if(controller == 1){
 
-  // Altera a posição das nuvens
-  positionC_x = positionC_x + speed_cloud;
-  positionCC_x = positionCC_x + speed_cloud;
+    // Altera a posição do feixe de luz
+    positionL_x = positionL_x - speed_light;
 
-  // Altera a posição dos pequenos passaros
-  positionB_x = positionB_x + speed_little;
+    // Altera a posição das nuvens
+    positionC_x = positionC_x + speed_cloud;
+    positionCC_x = positionCC_x + speed_cloud;
 
-  // Altera a posição do chão
-  positionF_x = positionF_x + speed_floor;
-  positionF2_x = positionF2_x + speed_floor;
+    // Altera a posição dos pequenos passaros
+    positionB_x = positionB_x + speed_little;
 
-  // Reinicia a posição de cada imagem quando ela sai da tela
-  if (positionL_x <= -2560) {
-    positionL_x = 0;
-  }
+    // Altera a posição do chão
+    positionF_x = positionF_x + speed_floor;
+    positionF2_x = positionF2_x + speed_floor;
 
-  if (positionC_x >= 2560) {
+    // Reinicia a posição de cada imagem quando ela sai da tela
+    if (positionL_x <= -2560) {
+      positionL_x = 0;
+    }
+
+    if (positionC_x >= 2560) {
     positionC_x = 0;
-  }
-
-  if (positionCC_x >= 2560) {
-    positionCC_x = 0;
-  }
-
-  if (positionB_x >= 2560) {
-    positionB_x = 0;
-  }
-
-  if (positionF_x >= 2560) {
-    positionF_x = 0;
-  }
-
-  if (positionF2_x >= 2560) {
-    positionF2_x = 0;
-  }
-
-}
-// Atualiza a tela com os frames andados
-void Motion::draw() {
-
-  // Redesenhando cada imagem com a nova posição
-  al_draw_bitmap(background, 0, 0, 0);
-
-  // Animação dos passaros do fundo batendo as asas
-  // Mesma logica do Bird que bate as asas
-  if ((positionB_x <= cont) && (value == 0)) {
-    al_draw_bitmap(little, (positionB_x - 1280), 0, 0);
-
-    if (positionB_x == cont) {
-      cont = cont + 9;
-      value++;
     }
 
-  } else if ((positionB_x <= cont) && (value == 1)) {
-    al_draw_bitmap(little2, (positionB_x - 1280), 2, 0);
-    if (positionB_x == cont) {
-      cont = cont + 9;
-      value++;
+    if (positionCC_x >= 2560) {
+      positionCC_x = 0;
     }
-  } else if ((positionB_x <= cont) && (value == 2)) {
-    al_draw_bitmap(little3, (positionB_x - 1280), 4, 0);
 
-    if (positionB_x == cont) {
-      cont = cont + 9;
-      value = 0;
+    if (positionB_x >= 2560) {
+      cont = 0;
+      positionB_x = 0;
     }
+
+    if (positionF_x >= 2560) {
+      positionF_x = 0;
+    }
+
+    if (positionF2_x >= 2560) {
+      positionF2_x = 0;
+    }
+  
   }
 
-  al_draw_bitmap(clouds2, (positionCC_x - 1280), 0, 0);
-  al_draw_bitmap(clouds, (positionC_x - 1280), 0, 0);
-  al_draw_bitmap(ground, (positionF_x - 1280), 0, 0);
-  al_draw_bitmap(ground2, (positionF2_x - 1280), 0, 0);
-  al_draw_bitmap(lights, (positionL_x + 1280), -20, 0);
-}
-
-//Metodo que muda o clima para neve, mudando o background e fazendo nevar na tela
-void Motion::snow_update(){
+  //Metodo que muda o clima para neve, mudando o background e fazendo nevar na tela
+  if(controller == 2){
 
     //Atualiza a posição a cada frame
     positionF = positionF + speedFlakes;
@@ -110,13 +111,108 @@ void Motion::snow_update(){
         positionF3 = 550;
     }
 
+  }
+
+  if(controller == 3){
+
+    contF++;
+    positionR1 += speedRain;
+
+    positionR2 += speedRain;
+
+    if(positionR1 >= 2000){
+
+      positionR1 = 500;
+    }
+
+    if(positionR2 >= 2000){
+
+      positionR2 = 500;
+    }
+  }
 }
 
-//Método que atualiza a posição de cada desenho
-void Motion::snow_draw(){
+// Atualiza a tela com os frames andados
+void Motion::draw() {
 
-    al_draw_bitmap(background_snow, 0, 0, 0);
-    al_draw_bitmap(flakesLittle, 0, positionF - 1280, 0);
-    al_draw_bitmap(flakesLittle2, 0, positionF3 - 1280, 0);
-    al_draw_bitmap(flakesBig, 0, positionF2 - 1280, 0);
+  if(controller == 1){
+    // Redesenhando cada imagem com a nova posição
+    background->Draw(0, 0);
+
+    // Animação dos passaros do fundo batendo as asas
+    // Mesma logica do Bird que bate as asas
+    if ((positionB_x <= cont) && (value == 0)) {
+      little->Draw(positionB_x -1280, 0);
+
+      if (positionB_x == cont) {
+        cont = cont + 9;
+        value++;
+      }
+
+    } 
+    else if ((positionB_x <= cont) && (value == 1)) {
+      little2->Draw(positionB_x - 1280, 2);
+      if (positionB_x == cont) {
+        cont = cont + 9;
+        value++;
+      }
+    } 
+    else if ((positionB_x <= cont) && (value == 2)) {
+      little3->Draw(positionB_x - 1280, 4);
+
+      if (positionB_x == cont) {
+        cont = cont + 9;
+        value = 0;
+      }
+    }
+
+    clouds2->Draw(positionCC_x - 1280, 0);
+    clouds->Draw(positionC_x - 1280, 0);
+    ground->Draw(positionF_x - 1280, 0);
+    ground2->Draw(positionF2_x - 1280, 0);
+    lights->Draw(positionL_x + 1280, -20);
+
+  }
+
+  if(controller == 2){
+
+    background_snow->Draw(0, 0);
+    flakesLittle->Draw(0, positionF - 1280);
+    flakesLittle2->Draw(0, positionF3 - 1280);
+    flakesBig->Draw(0, positionF2 - 1280);
+
+
+  }
+
+  if(controller == 3){
+
+    background_rain->Draw(0, 0);
+
+    if((contF <= contB+20) && (contF >= contB) && (controll == 0)){
+
+      thunder->Draw(0, 0);
+      //std::cerr << "1" << std::endl;
+
+      if(contF == contB+20){
+        controll = 1;
+        contB += 50;
+      }
+    }
+
+    if((contF <= contB+20) && (contF >= contB) && (controll == 1)){
+
+      thunder1->Draw(0, 0);
+      //std::cerr << "2" << std::endl;
+
+      if(contF == contB+20){
+        controll = 0;
+        contB += 1000;
+      }
+    }
+
+    drips1->Draw(0, positionR1 - 1280);
+    drips2->Draw(0, positionR2 - 1280);
+    //std::cerr << contB << " " << contF << std::endl;
+  }
+
 }
